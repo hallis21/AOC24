1d1a4fa37e9dc481a06b43ce6bd6a670f103ab88
Halvor <halvor.ringsby@intility.no>
2024-12-03 13:44:07 +0100

    one line 3

--- 3/3.py
+++ 3/3.py
@@ -1,28 +1,5 @@
-import re
-inp = [x.strip() for x in open("in").readlines()]
+#1
+print(sum([sum([[(int(n1)*int(n2)) for n1, n2 in [match.replace("mul(", "").replace(")", "").split(",")]][0] for match in matches]) for matches in [__import__("re").compile(r"(mul\(\d{1,3},\d{1,3}\))").findall(line) for line in [x.strip() for x in open("in").readlines()]]]))
 
-n = 0
-enabled = True
-for line in inp:
-
-
-    matches = re.compile(r"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))").findall(line)
-    
-    
-    for match in matches:
-        
-        if match[2] != "":
-            enabled = False
-            continue
-        if match[1] != "":
-            enabled = True
-            continue   
-            
-            
-        if enabled:     
-            match = match[0].replace("mul(", "").replace(")", "")
-            n1, n2 = match.split(",")
-            n += int(n1) * int(n2)
-
-
-print(n)
+# 2
+print([sum([sum([(vars.__setitem__("e", True), 0)[1] if match[1] != "" else (vars.__setitem__("e", False), 0)[1] if match[2] != "" else [(int(n1)*int(n2)) for n1, n2 in [match[0].replace("mul(", "").replace(")", "").split(",")]][0] if vars["e"] else 0 for match in matches]) for matches in [__import__("re").compile(r"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))").findall(line) for line in [x.strip() for x in open("in").readlines()]]]) for vars in [{"e": True,}]][0])
